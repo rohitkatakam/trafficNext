6ec41ae5f2c5d70697a147339d1370c92a056945
Rohit Katakam <rohitkatakam@gmail.com>
2024-12-18 12:42:21 -0800

    %

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <ul className="list-disc list-inside text-sm text-sky-800 text-left font-[family-name:var(--font-geist-mono)] space-y-2 bg-gray-300 p-4 rounded-lg">
           <li>Upload some PNG picture of a traffic sign (this model is trained on EU traffic images)</li>
           <li>The saved Pytorch CNN called through Flask and hosted on Render, so the initial upload will take a minute or two</li>
-          <li>Due to somewhat limited training data (found with labels on Kaggle), the accuracy of this model is ~70</li>
+          <li>Due to somewhat limited training data (found with labels on Kaggle), the accuracy of this model is ~70%</li>
           <li>This model was trained using two convolutional layers (with Leaky ReLu as activation) and one hidden layer</li>
           <li>Training used Adam gradient descent until validation accuracy stopped increasing to prevent overfitting</li>
         </ul>
